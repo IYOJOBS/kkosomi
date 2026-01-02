44fc36de311efcef8ce863bf4727a3f606f2e013
IYOJOBS <rlaxodus465@gmail.com>
2026-01-03 00:53:54 +0900

    Update action.js

--- assets/js/action.js
+++ assets/js/action.js
@@ -1,8 +1,6 @@
-console.log("action.js loaded")
-
-async function saveToServer(state) {
+async function saveToServer(state, bj) {
   await fetch(
-    "https://broken-mode-e7b0.rlaxodus465.workers.dev/save",
+    `https://broken-mode-e7b0.rlaxodus465.workers.dev/save?bj=${bj}`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
